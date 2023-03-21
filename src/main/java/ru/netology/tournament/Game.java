faa201979b3b4e45f113a355a58ff70b594a87d1
Zlobryak <clychnik@gmail.com>
2023-03-21 08:47:47 +0300

    Some unnecessary cycles removed

--- src/main/java/ru/netology/tournament/Game.java
+++ src/main/java/ru/netology/tournament/Game.java
@@ -2,7 +2,7 @@
 package ru.netology.tournament;
 
 import java.util.HashMap;
-import java.util.Objects;
+
 import ru.netology.sys.AlreadyRegisteredException;
 import ru.netology.sys.NotRegisteredException;
 import ru.netology.sys.Player;
@@ -24,24 +24,13 @@ public class Game {
   public HashMap<String, Integer> findAll() {
     return allRegisteredPlayers;
   }
-
   public int round(String playerName1, String playerName2) {
     int winnerIs = 0;
-    int strength1 = 0;
-    int strenght2 = 0;
-
-    for (String player : allRegisteredPlayers.keySet()) {
-      if (Objects.equals(player, playerName1)) {
-        strength1 = allRegisteredPlayers.get(player);
-      } else if (Objects.equals(player, playerName2)) {
-        strenght2 = allRegisteredPlayers.get(player);
-      }
-    }
-    if (strength1 != 0) {
-      if (strenght2 != 0) {
-        if (strength1 > strenght2) {
+    if (allRegisteredPlayers.get(playerName1) != null) {
+      if (allRegisteredPlayers.get(playerName2) != null) {
+        if (allRegisteredPlayers.get(playerName1) > allRegisteredPlayers.get(playerName2)) {
           winnerIs = 1;
-        } else if (strength1 < strenght2) {
+        } else if (allRegisteredPlayers.get(playerName1) < allRegisteredPlayers.get(playerName2)) {
           winnerIs = 2;
         }
       } else {
